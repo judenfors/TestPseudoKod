273da3ee437bb852a3a9973c79de0d7767d02623
Admin <Admin@TEB127028>
2022-11-11 10:39:59 +0100

    Add project files.

---
+++ TestPseudoKod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestPseudoKod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int tal = int.Parse(Console.ReadLine());
+
+            if(tal >= 18)
+            {
+                Console.WriteLine("Du är myndig");
+            }
+
+            else{
+                Console.WriteLine("Du är inte myndig");
+            }
+
+        }
+    }
+}
+
